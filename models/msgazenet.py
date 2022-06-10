f94908d00831a0a905ad731798a48d7ed11e594d
z-mahmud22 <107276459+z-mahmud22@users.noreply.github.com>
2022-06-10 16:18:43 -0400

    Create msgazenet.py

---
+++ models/msgazenet.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  1 17:35:03 2022
+
+@author: zunayed mahmud
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import math
+import numpy as np
+from copy import deepcopy
+
+
+momentum = 0.001
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_planes, out_planes, stride, drop_rate=0.0, activate_before_residual=False):
+        super(BasicBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes, momentum=0.001, eps=0.001)
+        self.relu1 = nn.LeakyReLU(negative_slope=0.1, inplace=False)
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                               padding=1, bias=True)
+        self.bn2 = nn.BatchNorm2d(out_planes, momentum=0.001, eps=0.001)
+        self.relu2 = nn.LeakyReLU(negative_slope=0.1, inplace=False)
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1,
+                               padding=1, bias=True)
+        self.drop_rate = drop_rate
+        self.equalInOut = (in_planes == out_planes)
+        self.convShortcut = (not self.equalInOut) and nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride,
+                                                                padding=0, bias=True) or None
+        self.activate_before_residual = activate_before_residual
+
+    def forward(self, x):
+        if not self.equalInOut and self.activate_before_residual == True:
+            x = self.relu1(self.bn1(x))
+        else:
+            out = self.relu1(self.bn1(x))
+        out = self.relu2(self.bn2(self.conv1(out if self.equalInOut else x)))
+        if self.drop_rate > 0:
+            out = F.dropout(out, p=self.drop_rate, training=self.training)
+        out = self.conv2(out)
+        return torch.add(x if self.equalInOut else self.convShortcut(x), out)
+
+
+class NetworkBlock(nn.Module):
+    def __init__(self, nb_layers, in_planes, out_planes, block, stride, drop_rate=0.0, activate_before_residual=False):
+        super(NetworkBlock, self).__init__()
+        self.layer = self._make_layer(
+            block, in_planes, out_planes, nb_layers, stride, drop_rate, activate_before_residual)
+
+    def _make_layer(self, block, in_planes, out_planes, nb_layers, stride, drop_rate, activate_before_residual):
+        layers = []
+        for i in range(int(nb_layers)):
+            layers.append(block(i == 0 and in_planes or out_planes, out_planes,
+                                i == 0 and stride or 1, drop_rate, activate_before_residual))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layer(x)
+        
+class msgazenet(nn.Module):
+    def __init__(self, first_stride, num_classes, depth=28, widen_factor=2, drop_rate=0.0, is_remix=False):
+        super(msgazenet, self).__init__()
+        channels = [16, 16 * widen_factor, 32 * widen_factor, 64 * widen_factor]
+        assert ((depth - 4) % 6 == 0)
+        n = (depth - 4) / 6
+        block = BasicBlock
+        # 1st conv before any network block
+        self.conv1_eye = nn.Conv2d(1, channels[0], kernel_size=3, stride=1,
+                               padding=1, bias=True)
+        self.conv1_iris = nn.Conv2d(1, channels[0], kernel_size=3, stride=1,
+                               padding=1, bias=True)
+        self.conv1_eyemask = nn.Conv2d(1, channels[0], kernel_size=3, stride=1,
+                               padding=1, bias=True)
+        # 1st block
+        self.block1_eye = NetworkBlock(
+            n, channels[0], channels[1], block, first_stride, drop_rate, activate_before_residual=True)
+        self.block1_iris = NetworkBlock(
+            n, channels[0], channels[1], block, first_stride, drop_rate, activate_before_residual=True)
+        self.block1_eyemask = NetworkBlock(
+            n, channels[0], channels[1], block, first_stride, drop_rate, activate_before_residual=True)
+        # 2nd block
+        self.block2_eye = NetworkBlock(
+            n, channels[1], channels[2], block, 2, drop_rate)
+        self.block2_iris = NetworkBlock(
+            n, channels[1], channels[2], block, 2, drop_rate)
+        self.block2_eyemask = NetworkBlock(
+            n, channels[1], channels[2], block, 2, drop_rate)
+        # 3rd block
+        self.block3 = NetworkBlock(
+            n, channels[2]*3, channels[3], block, 2, drop_rate)
+        # global average pooling and classifier
+        self.bn1 = nn.BatchNorm2d(channels[3], momentum=0.001, eps=0.001)
+        self.relu = nn.LeakyReLU(negative_slope=0.1, inplace=False)
+        self.fc = nn.Sequential(
+                  nn.Linear(channels[3], channels[3]*2),
+                  nn.ReLU(inplace=True),
+                  nn.Dropout(0.25),
+                  nn.Linear(channels[3]*2, channels[3]),
+                  nn.ReLU(inplace=True),
+                  nn.Dropout(0.25),
+                  nn.Linear(channels[3], num_classes)
+                  )
+        self.channels = channels[3]
+
+        # rot_classifier for Remix Match
+        self.is_remix = is_remix
+        if is_remix:
+            self.rot_classifier = nn.Linear(self.channels, 4)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight.data)
+                m.bias.data.zero_()
+   
+    def forward(self, x, ood_test=False):
+        out_x = self.conv1_eye(x['eye'])
+        out_y = self.conv1_iris(x['iris'])
+        out_z = self.conv1_eyemask(x['eye_mask'])
+        out_x = self.block1_eye(out_x)
+        out_y = self.block1_iris(out_y)
+        out_z = self.block1_eyemask(out_z)
+        out_x = self.block2_eye(out_x)
+        out_y = self.block2_iris(out_y)
+        out_z = self.block2_eyemask(out_z)
+        out = torch.cat((out_x, out_y, out_z),1)
+        out = self.block3(out)
+        out = self.relu(self.bn1(out))
+        out = F.adaptive_avg_pool2d(out, 1)
+        out = out.view(-1, self.channels)
+        output = self.fc(out)
+
+        return output
+                
+                
+class build_msgazenet:
+    def __init__(self, first_stride=1, depth=28, widen_factor=2, bn_momentum=0.01, leaky_slope=0.0, dropRate=0.0,
+                 use_embed=False, is_remix=False):
+        self.first_stride = first_stride
+        self.depth = depth
+        self.widen_factor = widen_factor
+        self.bn_momentum = bn_momentum
+        self.dropRate = dropRate
+        self.leaky_slope = leaky_slope
+        self.use_embed = use_embed
+        self.is_remix = is_remix
+
+    def build(self, num_classes):
+        return msgazenet(
+            first_stride=self.first_stride,
+            depth=self.depth,
+            num_classes=num_classes,
+            widen_factor=self.widen_factor,
+            drop_rate=self.dropRate,
+            is_remix=self.is_remix,
+        )
+        
+        
